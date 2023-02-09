f2e46986c7fb6f410c54dbbd8fc48c25e22bfe17
nut_jeep <anajibmuhammad@gmail.com>
2023-02-09 09:23:42 +0700

    delete seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,26 +18,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'username'  => 'Admin',
-            'password'  => bcrypt('idhamperkasa')
-        ]);
         
-        About::create([
-            'about' => 'CV. IDHAM PERKASA sebagai perusahaan yang bergerak di bidang Rubber Product Manufacturing (Seal, Anti Slip Pallet, Membran, dll). Dengan didukung oleh sumber daya manusia yang berpengalaman, ditunjang dengan mesin cetak & CNC modern. Selain manufaktur Rubber Product, CV. IDHAM PERKASA memiliki stock barang penjualan seperti Seal NOK, TTO, CHO, NTK, dll.',
-            'company' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos, illo dolore sint explicabo totam hic aut aspernatur quidem, maiores labore, fuga nihil! Quas officia dolore doloribus nemo laborum, perferendis impedit nostrum veritatis obcaecati fuga quod nesciunt unde quo molestiae blanditiis qui molestias nam ullam. Iure, fugiat. Facilis obcaecati nesciunt repudiandae?',
-            'visi'  => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit expedita minus voluptates quas distinctio? Totam blanditiis aperiam dolores consequuntur temporibus. Eum deleniti vero quasi maxime! Repudiandae, consequuntur nisi sequi recusandae dignissimos cum illo consectetur esse illum, ratione maiores voluptas dolorem. Repellat, delectus eius dicta enim a culpa quibusdam inventore totam.',
-            'misi'  => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laudantium obcaecati, itaque nemo repellendus atque fugiat eius pariatur assumenda iusto harum perferendis blanditiis, minus debitis nulla quae officiis ab vero.'
-        ]); 
-
-        Category::create([
-            'category_name'  => 'Seal'
-        ]);
-        Category::create([
-            'category_name'  => 'Safety'
-        ]);
-        Category::create([
-            'category_name'  => 'Iron'
-        ]);
     }
 }
